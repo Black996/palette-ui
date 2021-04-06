f4d5b0b3ef306867974753dbf331c9d96afacef9
Black996 <rharasis@yahoo.com>
2021-04-06 22:13:49 +0300

    added Palette + seedColors files

---
+++ src/Palette.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Palette extends Component {
+    render() {
+        return (
+            <div className="Palette">
+                {/* {Navbar here} */}
+                <div className="Palette-colors"></div>
+                {/*A bunch of color boxes */}
+                {/*A footer */}
+            </div>
+        )
+    }
+}
+
+export default Palette
