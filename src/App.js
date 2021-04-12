320e1aac35d2a797b453e08eadcf372765a8c15b
Black996 <rharasis@yahoo.com>
2021-04-12 15:24:17 +0300

    Add Link to ColorBox and Add Route

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ class App extends Component {
         <Route
           exact path="/palette/:id"
           render={(routeProps) => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />} />
+        <Route exact path="/palette/:paletteId/:colorId" render={() => <h1>Single Color Page</h1>} />
       </Switch>
 
 
