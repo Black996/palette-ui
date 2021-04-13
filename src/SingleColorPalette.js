e55091b40513817120f5372195ae20d7972fec1d
Black996 <rharasis@yahoo.com>
2021-04-13 22:31:15 +0300

    Move out styles

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -4,47 +4,7 @@ import ColorBox from "./ColorBox";
 import Navbar from "./Navbar";
 import { withStyles } from "@material-ui/styles";
 import PaletteFooter from './PaletteFooter';
-
-const styles = {
-    Palette: {
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column"
-    },
-    colors: {
-        height: "90%"
-    },
-    goBack: {
-        width: "20%",
-        height: props => props.showingFullPalette ? "25%" : "50%",
-        margin: "0 auto",
-        display: "inline-block",
-        position: "relative",
-        cursor: "pointer",
-        marginBottom: "-4px",
-        opacity: "1",
-        backgroundColor: "black",
-        "& a": {
-            color: "white",
-            width: "100px",
-            height: "30px",
-            position: " absolute",
-            display: "inline-block",
-            top: "50%",
-            left: "50%",
-            marginLeft: "-50px",
-            marginTop: "-15px",
-            textAlign: "center",
-            outline: "none",
-            backgroundColor: "rgba(255, 255, 255, 0.3)",
-            fontSize: "1rem",
-            lineHeight: "30px",
-            textTransform: "uppercase",
-            border: "none",
-            textDecoration: "none",
-        }
-    }
-}
+import styles from "./styles/PaletteStyles";
 
 
 class SingleColorPalette extends Component {
