9e12a5cf537bfb83be37d7419d744018876b1437
Black996 <rharasis@yahoo.com>
2021-04-18 02:16:09 +0300

    Move styles out and add constants file

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
 import PaletteFormNav from "./PaletteFormNav";
 import ColorPickerForm from "./ColorPickerForm";
 import Drawer from '@material-ui/core/Drawer';
@@ -11,65 +10,66 @@ import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import { Button } from '@material-ui/core';
 import DraggableColorList from "./DraggableColorList";
 import { arrayMove } from 'react-sortable-hoc';
+import useStyles from "./styles/NewPaletteFormStyles"
 
-const drawerWidth = 350;
+// const drawerWidth = 350;
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-    },
-    hide: {
-        display: 'none',
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-    },
-    drawerPaper: {
-        width: drawerWidth,
-        display: "flex",
-        alignItems: "center"
-    },
-    drawerHeader: {
-        display: 'flex',
-        alignItems: 'center',
-        padding: theme.spacing(0, 1),
-        // necessary for content to be below app bar
-        ...theme.mixins.toolbar,
-        justifyContent: 'flex-end',
-    },
-    content: {
-        flexGrow: 1,
-        height: "calc(100vh - 64px)",
-        padding: theme.spacing(3),
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        marginLeft: -drawerWidth,
-    },
-    contentShift: {
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-        marginLeft: 0,
-    },
-    container: {
-        width: "90%",
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    buttons: {
-        width: "100%"
-    },
-    button: {
-        width: "50%"
-    }
-}));
+// const useStyles = makeStyles((theme) => ({
+//     root: {
+//         display: 'flex',
+//     },
+//     hide: {
+//         display: 'none',
+//     },
+//     drawer: {
+//         width: drawerWidth,
+//         flexShrink: 0,
+//     },
+//     drawerPaper: {
+//         width: drawerWidth,
+//         display: "flex",
+//         alignItems: "center"
+//     },
+//     drawerHeader: {
+//         display: 'flex',
+//         alignItems: 'center',
+//         padding: theme.spacing(0, 1),
+//         // necessary for content to be below app bar
+//         ...theme.mixins.toolbar,
+//         justifyContent: 'flex-end',
+//     },
+//     content: {
+//         flexGrow: 1,
+//         height: "calc(100vh - 64px)",
+//         padding: theme.spacing(3),
+//         transition: theme.transitions.create('margin', {
+//             easing: theme.transitions.easing.sharp,
+//             duration: theme.transitions.duration.leavingScreen,
+//         }),
+//         marginLeft: -drawerWidth,
+//     },
+//     contentShift: {
+//         transition: theme.transitions.create('margin', {
+//             easing: theme.transitions.easing.easeOut,
+//             duration: theme.transitions.duration.enteringScreen,
+//         }),
+//         marginLeft: 0,
+//     },
+//     container: {
+//         width: "90%",
+//         height: "100%",
+//         display: "flex",
+//         flexDirection: "column",
+//         justifyContent: "center",
+//         alignItems: "center"
+//     },
+//     buttons: {
+//         width: "100%"
+//     },
+//     button: {
+//         width: "50%"
+//     }
+// }));
 
 
 NewPaletteForm.defaultProps = {
--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
 import PaletteMetaForm from "./PaletteMetaForm";
 import { Link } from "react-router-dom";
 import clsx from 'clsx';
@@ -10,38 +9,9 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { Button } from '@material-ui/core';
-import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
+import useStyles from "./styles/PaletteFormNavStyles";
 
-const drawerWidth = 350;
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: "flex"
-    },
-    appBar: {
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        flexDirection: "row",
-        justifyContent: "space-between",
-        height: "64px"
-    },
-    appBarShift: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: drawerWidth,
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    navBtns: {
-
-    }
-}))
 
 
 function PaletteFormNav(props) {
